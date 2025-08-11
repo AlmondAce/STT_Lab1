507747d2654f7acc2bce07e362c102acfafbf646
AlmondAce <akshat.pratapsingh@iitgn.ac.in>
2025-08-11 12:55:14 +0530

    added code in test.py

---
+++ test.py
@@ -0,0 +1,28 @@
+# Printing some messages
+print("Hello, World!")
+print("Welcome to my simple Python program.")
+print("Let's do some math operations.\n")
+
+# Addition
+a = 10
+b = 5
+sum_result = a + b
+print("Addition of", a, "and", b, "is:", sum_result)
+
+# Subtraction
+sub_result = a - b
+print("Subtraction of", a, "and", b, "is:", sub_result)
+
+# Multiplication
+mul_result = a * b
+print("Multiplication of", a, "and", b, "is:", mul_result)
+
+# Division
+div_result = a / b
+print("Division of", a, "and", b, "is:", div_result)
+
+print("\nNow let's use a loop to print numbers from 1 to 5.")
+for i in range(1, 6):
+    print(i)
+
+print("\nProgram finished successfully.")
